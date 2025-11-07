3687525fc3a56a4c4fed55b940d9220d51821701
Gustavo <=>
2025-11-07 17:48:54 -0300

    feat: Configure RabbitMQ microservices for API Gateway and Auth Service

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
-  await app.listen(process.env.port ?? 3000);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://guest:guest@localhost:5672'],
+      queue: 'api_gateway',                        
+      queueOptions: {
+        durable: false,                            
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(process.env.API_GATEWAY_PORT || 3000);
+
+  console.log(`🚀 API Gateway corriendo en http://localhost:${process.env.API_GATEWAY_PORT || 3000}`);
+  console.log(`📡 Conectado a RabbitMQ en amqp://localhost:5672`);
 }
+
 bootstrap();
