355aa3e4247e68aa1f2d4132c57091e08f3d6fb0
Gustavo <=>
2025-11-07 18:40:15 -0300

    feat: Enhance Users Service with TypeORM integration and user role management

--- apps/users-service/src/users-service.module.ts
+++ apps/users-service/src/users-service.module.ts
@@ -2,13 +2,29 @@ import { Module } from '@nestjs/common';
 import { UsersServiceController } from './users-service.controller';
 import { UsersServiceService } from './users-service.service';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService): TypeOrmModuleOptions => ({
+        type: config.get<'postgres'>('DB_TYPE') as any,
+        host: config.get<string>('DB_HOST'),
+        port: +config.get('DB_PORT') || 5432,
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: config.get<string>('DB_SYNCHRONIZE') === 'true',
+        logging: config.get<string>('DB_LOGGING') === 'true',
+      }),
+      inject: [ConfigService],
     }),
     UsersModule
   ],
--- apps/users-service/src/users/entities/user.entity.ts
+++ apps/users-service/src/users/entities/user.entity.ts
@@ -1,1 +1,29 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+export enum UserRole {
+    ADMIN = 'Administrator',
+    SUPPORT = 'Support',
+    USER = 'User',
+}
+
+@Entity()
+export class UserEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    password: string;
+
+    @Column({
+        type: 'enum',
+        enum: UserRole,
+        default: UserRole.USER,
+    })
+    role: UserRole;
+}
