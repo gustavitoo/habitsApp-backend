162c81f39424fede6a779003632ad3894b4a44cb
Gustavo <=>
2025-11-07 17:55:31 -0300

    feat: Refactor Users Service to use RabbitMQ microservice transport

--- apps/users-service/src/main.ts
+++ apps/users-service/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { UsersServiceModule } from './users-service.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(UsersServiceModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(UsersServiceModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://guest:guest@localhost:5672'],
+      queue: 'users_queue',
+      queueOptions: { durable: false },
+    },
+  });
+
+  await app.listen();
+  console.log('✅ Users Service conectado a RabbitMQ');
 }
+
 bootstrap();
